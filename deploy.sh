f8b1a025c0d39f62a07d4d627ce857b48a9f0b69
VanikKhachatryan2002 <vanik_khachatryan_work@mail.ru>
2024-10-04 00:04:26 +0400

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 set -e
 
-# Your deployment steps
+# Ensure the script is executable
+chmod +x deploy.sh
+
+# Deployment steps
 composer install --no-dev --optimize-autoloader
 php artisan key:generate
 npm install
 npm run prod
 php artisan cache:clear
 
-# Start the PHP-FPM server (if you're using a setup with Nginx)
+# Start the PHP-FPM server
 php-fpm

5e5289827e830c56e1c660e09625c26a4180154c
VanikKhachatryan2002 <vanik_khachatryan_work@mail.ru>
2024-10-19 23:43:46 +0400

    deploy

--- config/database.php
+++ config/database.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -66,11 +66,11 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'dpg-cs8kgd5svqrc73bte2v0-a.oregon-postgres.render.com'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'kkf_database'),
+            'username' => env('DB_USERNAME', 'vanik'),
+            'password' => env('DB_PASSWORD', 'bx3N044mytVAevoIqWwZd9Q914i7EUFQ'),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
